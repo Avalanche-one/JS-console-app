2af4c5c0927ce3ea936555a82651acf56fafa47d
Nick <avalanche3122@gmail.com>
2021-06-07 13:30:54 +0300

    modified app with functions

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 'use strict';
-
-const numberOfFilms = +prompt('Скільки фільмів ви переглянули?', '');
+//functions and variable
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt('Скільки фільмів ви переглянули?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Скільки фільмів ви переглянули?', '');
+    }
+}
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один з останніх переглянутих фільмів?', '');
+        const b = prompt('На скільки оціните його?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('OK');
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Low');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Classic');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Cinema lover');
+    } else {
+        console.log('Error');
+    }
+}
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i - 1] = prompt(`Your best genre number ${i}?`);
+    }
+}
+//app is going
+start();
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    privat: false
 };
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один з останніх переглянутих фільмів?', '');
-    const b = prompt('На скільки оціните його?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50){
-        personalMovieDB.movies[a] = b;
-        console.log('OK');
-    } else {
-        console.log('Error');
-        i--;
-    }
-}
-if (personalMovieDB.count < 10) {
-    console.log('Low');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Classic');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Cinema lover');
-} else {
-    console.log('Error');
-}
-console.log(personalMovieDB);
+rememberMyFilms();
+detectPersonalLevel();
+writeYourGenres();
+showMyDB(personalMovieDB.privat);
+
 
 
 
